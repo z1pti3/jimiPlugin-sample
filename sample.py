4bd214e3e0667a580bea5b7ed5f5ee75a8813b26
z1pti3 <66521110+z1pti3@users.noreply.github.com>
2021-12-07 20:47:37 +0000

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,9 +4,9 @@ class _sample(jimi.plugin._plugin):
     version = 0.1
 
     def install(self):
-        jimi.model.registerModel("sampleAction","_sampleAction","_action","plugins.sample.models.webTrigger")
+        jimi.model.registerModel("sampleAction","_sampleAction","_action","plugins.sample.models.action")
         return True
 
     def uninstall(self):
-        jimi.model.deregisterModel("sampleAction","_sampleAction","_action","plugins.sample.models.webTrigger")
+        jimi.model.deregisterModel("sampleAction","_sampleAction","_action","plugins.sample.models.action")
         return True
