5ca09cdad46dd419793507b09a7e8b47f4338f32
z1pti3 <66521110+z1pti3@users.noreply.github.com>
2021-12-07 20:47:23 +0000

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,12 @@
+import jimi
+
+class _sample(jimi.plugin._plugin):
+    version = 0.1
+
+    def install(self):
+        jimi.model.registerModel("sampleAction","_sampleAction","_action","plugins.sample.models.webTrigger")
+        return True
+
+    def uninstall(self):
+        jimi.model.deregisterModel("sampleAction","_sampleAction","_action","plugins.sample.models.webTrigger")
+        return True
